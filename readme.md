b1be09a77b3b6a955199d6ba3d35c177006ae5a1
Johan van der Knijff <johan.vanderknijff@kb.nl>
2015-09-04 17:37:40 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# verifyISOSize
+
+## About
+*verifyISOSize* verifies if the file size of a CD-ROM ISO 9660 image is consistent with the information in its Volume Descriptors. This can be useful for detecting incomplete (e.g. truncated) ISO images. 
---
+++ verifyISOSize/verifyISOSize.py
@@ -0,0 +1,130 @@
+#! /usr/bin/env python
+# 
+# Verify if size of CD-ROM ISO image matches information in
+# ISO 9660 Volume Descriptors (no support for UDF file systems yet)
+# 
+#
+# Copyright (C) 2015, Johan van der Knijff, Koninklijke Bibliotheek -
+#  National Library of the Netherlands
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import os
+import time
+import imp
+import glob
+import struct
+import argparse
+import byteconv as bc
+scriptPath, scriptName = os.path.split(sys.argv[0])
+
+__version__= "0.1.0"
+
+# Create parser
+parser = argparse.ArgumentParser(
+    description="Verify file size of ISO 9660")
+
+def main_is_frozen():
+    return (hasattr(sys, "frozen") or # new py2exe
+        hasattr(sys, "importers") # old py2exe
+        or imp.is_frozen("__main__")) # tools/freeze
+
+def get_main_dir():
+    if main_is_frozen():
+        return os.path.dirname(sys.executable)
+    return os.path.dirname(sys.argv[0])
+    
+def printWarning(msg):
+    msgString=("User warning: " + msg +"\n")
+    sys.stderr.write(msgString)
+
+def errorExit(msg):
+    msgString=("Error: " + msg + "\n")
+    sys.stderr.write(msgString)
+    sys.exit()
+
+def checkFileExists(fileIn):
+    # Check if file exists and exit if not
+    if os.path.isfile(fileIn)==False:
+        msg=fileIn + " does not exist"
+        errorExit(msg)
+    
+def readFileBytes(file):
+    # Read file, return contents as a byte object
+
+    # Open file
+    f = open(file,"rb")
+
+    # Put contents of file into a byte object.
+    fileData=f.read()
+    f.close()
+
+    return(fileData)
+
+def getVolumeDescriptor(bytesData, byteStart):
+
+    # Read one 2048-byte volume descriptor and return its descriptor
+    # code and contents
+    byteEnd = byteStart + 2048
+    volumeDescriptorType = bc.bytesToUnsignedChar(bytesData[byteStart:byteStart+1])
+    volumeDescriptorData = bytesData[byteStart:byteEnd]
+    
+    return(volumeDescriptorType, volumeDescriptorData, byteEnd)
+    
+def parseCommandLine():
+    # Add arguments
+    parser.add_argument('ISOImage', 
+        action = "store", 
+        type = str, 
+        help = "input ISO image")
+    parser.add_argument('--version', '-v',
+        action = 'version', 
+        version = __version__)
+
+    # Parse arguments
+    args=parser.parse_args()
+
+    return(args)
+
+def main():
+    # Get input from command line
+    args = parseCommandLine()
+     
+    # Input
+    ISOImage = args.ISOImage
+    
+    # Does input image exist?
+    checkFileExists(ISOImage)
+    
+    isoBytes = readFileBytes(ISOImage)
+    
+    # Skip bytes 0 - 32767 (system area, usually empty)
+    byteStart = 32768
+    
+    # This is a dummy value
+    volumeDescriptorType = -1
+   
+    
+    # Read through all 2048-byte volume descriptors, until Volume Descriptor Set Terminator is found
+    while volumeDescriptorType != 255:
+    
+        volumeDescriptorType, volumeDescriptorData, byteEnd = getVolumeDescriptor(isoBytes, byteStart)
+        byteStart = byteEnd
+        print(str(volumeDescriptorType))
+    
+    
+
+if __name__ == "__main__":
+    main()
+
