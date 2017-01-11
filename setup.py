373b28a727ab97319457e706f25dc316b378fdef
bitsgalore <johan.vanderknijff@kb.nl>
2017-01-11 16:11:45 +0100

    version number read directly from isolyzer.py (stole code from fido)

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import codecs
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
 
 readme = open('README.md', 'r')
 README_TEXT = readme.read()
 readme.close()
 
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding='utf-8') as fobj:
+        return fobj.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(name='isolyzer',
-      packages=['isolyzer'],
-      version='0.1.1',
+      packages=find_packages(),
+      version=find_version('isolyzer', 'isolyzer.py'),
       license='LGPL',
       platforms=['POSIX', 'Windows'],
       description='Verify size of ISO image',
@@ -16,8 +33,17 @@ setup(name='isolyzer',
       author='Johan van der Knijff',
       author_email='johan.vanderknijff@kb.nl',
       maintainer='Johan van der Knijff',
-      maintainer_email='johan.vanderknijff@kb.nl',
-      download_url = 'https://github.com/KBNLresearch/verifyISOSize/archive/0.1.1.tar.gz'
-      )
-
+      maintainer_email = 'johan.vanderknijff@kb.nl',
+      download_url='https://github.com/KBNLresearch/verifyISOSize/archive/0.1.1.tar.gz',
+      package_data={'isolyzer': ['*.*']},
+      entry_points={'console_scripts': [
+        'isolyzer = isolyzer:main',
+      ]},
+      classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
+    )
 
