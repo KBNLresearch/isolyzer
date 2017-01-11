224ad06c0b89678d84a3343690e7bb187be5b19a
bitsgalore <johan.vanderknijff@kb.nl>
2017-01-11 14:36:42 +0100

    added init file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+readme = open('README.md', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(name='isolyzer',
+      packages=['isolyzer'],
+      version='1.17.0',
+      license='LGPL',
+      platforms=['POSIX', 'Windows'],
+      description='Verify size of ISO image',
+      long_description=README_TEXT,
+      author='Johan van der Knijff',
+      author_email='johan.vanderknijff@kb.nl',
+      maintainer='Johan van der Knijff',
+      maintainer_email='johan.vanderknijff@kb.nl'
+      )
+
+
