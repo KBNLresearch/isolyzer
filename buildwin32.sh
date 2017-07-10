693fe40a3d3c0d7f2a8b7577387a56e2a6398cb6
bitsgalore <johan.vanderknijff@kb.nl>
2017-07-10 18:24:17 +0200

    first stab at Wine-based build script for Windows executables

---
+++ buildwin32.sh
@@ -0,0 +1,53 @@
+# Build Windows isolyzer binaries from Python script, and pack them in ZIP file
+
+#
+# Dependencies:
+# - Wine environment with Python, pyInstaller and all isolyzer dependencies
+# - a spec file
+
+# CONFIGURATION 
+
+# Python
+# Note that to produce a 32-bit binary we need a 32-bit Python version!
+pythonWine=~/.wine/drive_c/Python27/python.exe
+pyInstallerWine=~/.wine/drive_c/Python27/Scripts/pyinstaller.exe
+
+# Script base name (i.e. script name minus .py extension)
+scriptBaseName=isolyzer
+
+# PyInstaller spec file that defines build options
+specFile=win32.spec
+
+# Directory where build is created (should be identical to 'name' in 'coll' in spec file!!)
+distDir=./dist_win32/
+
+# Executes isolyzer with -v option and stores output to 
+# env variable 'version'
+wine $pythonWine ./$scriptBaseName/$scriptBaseName.py -v 2> temp.txt
+version=$(head -n 1 temp.txt)
+rm temp.txt
+
+# BUILD
+
+# Build binaries
+$pyInstallerWine $specFile
+
+# Generate name for ZIP file
+zipName="$scriptBaseName"_"$version"_win32.zip
+
+# Create ZIP file
+wine $pythonWine zipdir.py "$distDir"/"$scriptBaseName" "$distDir"/"$zipName"
+
+# CLEANUP 
+
+# Delete build directory
+#rmdir build /S /Q
+
+# Delete isolyzer directory in distdir
+#rmdir %distDir%\isolyzer /S /Q
+
+# PARTY TIME! 
+
+echo "Done!"
+
+
