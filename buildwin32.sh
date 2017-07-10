da6c073900e2e7cb3a8e8df9e802d8de546d4294
bitsgalore <johan.vanderknijff@kb.nl>
2017-07-10 18:49:49 +0200

    cleanup

--- buildwin32.sh
+++ buildwin32.sh
@@ -19,7 +19,7 @@ scriptBaseName=isolyzer
 specFile=win32.spec
 
 # Directory where build is created (should be identical to 'name' in 'coll' in spec file!!)
-# TOSO is this even used, as default name seems to be 'dist'?!
+# TODO is this even used, as default name seems to be 'dist'?!
 distDir=./dist_win32/
 
 # Executes isolyzer with -v option and stores output to 
@@ -27,8 +27,6 @@ distDir=./dist_win32/
 wine $pythonWine ./$scriptBaseName/$scriptBaseName.py -v 2> temp.txt
 version=$(head -n 1 temp.txt)
 rm temp.txt
-
-# BUILD
 
 # Build binaries
 $pyInstallerWine $specFile
@@ -40,18 +38,12 @@ zipName="$scriptBaseName"_"$version"_win32.zip
 cd ./dist
 zip -r $zipName isolyzer
 cd ..
-mv ./dist/$zipName .
 
 # CLEANUP 
 
 # Delete build directory
-#rmdir build /S /Q
-
-# Delete isolyzer directory in distdir
-#rmdir %distDir%\isolyzer /S /Q
-
-# PARTY TIME! 
+rm -r ./build
+rm -r  ./dist/$scriptBaseName
 
 echo "Done!"
-
 
