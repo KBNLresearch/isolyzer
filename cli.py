b5f3564abacbcee30da140c4206939937189e1a4
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-24 17:14:52 +0200

    added wrapper script

---
+++ cli.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+#
+"""CLI wrapper script, ensures that relative imports work correctly in a PyInstaller build"""
+
+from isolyzer.isolyzer import main
+
+if __name__ == '__main__':
+    main()
