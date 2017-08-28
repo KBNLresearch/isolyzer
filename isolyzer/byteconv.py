f1e0a57ae7ad33f40b58d1abab6f241129943141
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-28 16:23:39 +0200

    pyLint and PEP8 fixes for byteconv.py

--- isolyzer/byteconv.py
+++ isolyzer/byteconv.py
@@ -1,88 +1,93 @@
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#! /usr/bin/env python
+"""Various conversion functions from bytes to other data types"""
 
 import struct
 import binascii
 import unicodedata
 
 
-# Convert byte object of bOrder byteorder to format using formatCharacter
-# Return -9999 if unpack raised an error
-
-
 def _doConv(bytestr, bOrder, formatCharacter):
+    """Convert byte object of bOrder byteorder to format using formatCharacter
+    Return -9999 if unpack raised an error
+    """
     # Format string for unpack
     formatStr = bOrder + formatCharacter
     try:
         result = struct.unpack(formatStr, bytestr)[0]
     except:
         result = -9999
-    return(result)
+    return result
+
 
 def swap32(i):
-    # Byte-swap 4 byte integer (Credit: http://stackoverflow.com/a/27506692)
+    """ Byte-swap 4 byte integer (Credit: http://stackoverflow.com/a/27506692)"""
     return struct.unpack("<I", struct.pack(">I", i))[0]
 
-def bytesToULongLong(bytes):
-    # Unpack 8 byte string to unsigned long long integer, assuming big-endian
-    # byte order.
-    return _doConv(bytes, ">", "Q")
 
-def bytesToUInt(bytes):
-    # Unpack 4 byte string to unsigned integer, assuming big-endian byte order.
-    return _doConv(bytes, ">", "I")
+def bytesToULongLong(bytestring):
+    """Unpack 8 byte string to unsigned long long integer, assuming big-endian
+    byte order
+    """
+    return _doConv(bytestring, ">", "Q")
 
-def bytesToUShortInt(bytes):
-    # Unpack 2 byte string to unsigned short integer, assuming big-endian
-    # byte order
-    return _doConv(bytes, ">", "H")
 
-def bytesToUnsignedChar(bytes):
-    # Unpack 1 byte string to unsigned character/integer, assuming big-endian
-    # byte order.
-    return _doConv(bytes, ">", "B")
+def bytesToUInt(bytestring):
+    """Unpack 4 byte string to unsigned integer, assuming big-endian byte order"""
+    return _doConv(bytestring, ">", "I")
 
-def bytesToSignedChar(bytes):
-    # Unpack 1 byte string to signed character/integer, assuming big-endian
-    # byte order.
-    return _doConv(bytes, ">", "b")
 
-## Below the little-Endian equivalents of the above functions
+def bytesToUShortInt(bytestring):
+    """Unpack 2 byte string to unsigned short integer, assuming big-endian
+    byte order
+    """
+    return _doConv(bytestring, ">", "H")
 
-def bytesToULongLongL(bytes):
-    # Unpack 8 byte string to unsigned long long integer, assuming little-endian
-    # byte order.
-    return _doConv(bytes, "<", "Q")
 
-def bytesToUIntL(bytes):
-    # Unpack 4 byte string to unsigned integer, assuming little-endian byte order.
-    return _doConv(bytes, "<", "I")
+def bytesToUnsignedChar(bytestring):
+    """Unpack 1 byte string to unsigned character/integer, assuming big-endian
+    byte order
+    """
+    return _doConv(bytestring, ">", "B")
 
-def bytesToUShortIntL(bytes):
-    # Unpack 2 byte string to unsigned short integer, assuming little-endian
-    # byte order
-    return _doConv(bytes, "<", "H")
 
-def bytesToUnsignedCharL(bytes):
-    # Unpack 1 byte string to unsigned character/integer, assuming little-endian
-    # byte order.
-    return _doConv(bytes, "<", "B")
+def bytesToSignedChar(bytestring):
+    """Unpack 1 byte string to signed character/integer, assuming big-endian
+    byte order
+    """
+    return _doConv(bytestring, ">", "b")
 
-def bytesToInteger(bytes):
-    # Unpack byte string of any length to integer.
-    #
+
+# Below the little-Endian equivalents of the above functions
+
+def bytesToULongLongL(bytestring):
+    """Unpack 8 byte string to unsigned long long integer, assuming little-endian
+    byte order
+    """
+    return _doConv(bytestring, "<", "Q")
+
+
+def bytesToUIntL(bytestring):
+    """Unpack 4 byte string to unsigned integer, assuming little-endian byte order"""
+    return _doConv(bytestring, "<", "I")
+
+
+def bytesToUShortIntL(bytestring):
+    """Unpack 2 byte string to unsigned short integer, assuming little-endian
+    byte order
+    """
+    return _doConv(bytestring, "<", "H")
+
+
+def bytesToUnsignedCharL(bytestring):
+    """Unpack 1 byte string to unsigned character/integer, assuming little-endian
+    byte order
+    """
+    return _doConv(bytestring, "<", "B")
+
+
+def bytesToInteger(bytestring):
+    """Unpack byte string of any length to integer"""
+
     # Taken from:
     # http://stackoverflow.com/questions/4358285/
     #
@@ -93,47 +98,55 @@ def bytesToInteger(bytes):
     # Issue 3532 bugs.python.org
 
     try:
-        result = int(binascii.hexlify(bytes), 16)
+        result = int(binascii.hexlify(bytestring), 16)
     except:
         result = -9999
 
-    return (result)
+    return result
+
 
 def isctrl(c):
-    # Returns True if byte corresponds to device control character
+    """Returns True if byte corresponds to device control character"""
+
     # (See also: http://www.w3schools.com/tags/ref_ascii.asp)
-    return (ord(c) < 32 or ord(c) == 127)
-    # return (0 <= ord(c) <= 8) or (ord(c) == 12) or (14 <= ord(c) < 32)
+    return ord(c) < 32 or ord(c) == 127
 
-def bytesToHex(bytes):
-    # Return hexadecimal ascii representation of bytes
-    return binascii.hexlify(bytes)
 
-def containsControlCharacters(bytes):
-    # Returns True if bytes object contains control characters
+def bytesToHex(bytestring):
+    """Return hexadecimal ascii representation of bytestring"""
+    return binascii.hexlify(bytestring)
 
-    for i in range(len(bytes)):
-        if isctrl(bytes[i:i + 1]):
-            return(True)
-    return(False)
+
+def containsControlCharacters(bytestring):
+    """Returns True if bytestring object contains control characters"""
+
+    for i in range(len(bytestring)):
+        if isctrl(bytestring[i:i + 1]):
+            return True
+    return False
+
 
 def removeControlCharacters(string):
-    # Remove control characters from string
+    """Remove control characters from string"""
+
     # Adapted from: http://stackoverflow.com/a/19016117/1209004
-    
+
     # Tab, newline and return are part of C0, but are allowed in XML
-    allowedChars = [u'\t', u'\n',u'\r']
+    allowedChars = [u'\t', u'\n', u'\r']
     return "".join(ch for ch in string if unicodedata.category(ch)[0] != "C" or ch in allowedChars)
-    
-def removeNullTerminator(bytes):
-    # Remove null terminator from bytes
 
-    bytesOut = bytes.rstrip(b'\x00')
-    return(bytesOut)
 
-def bytesToText(bytes):
-    # Unpack byte object to text string, assuming big-endian
-    # byte order.
+def removeNullTerminator(bytestring):
+    """Remove null terminator from bytestring"""
+
+    bytesOut = bytestring.rstrip(b'\x00')
+    return bytesOut
+
+
+def bytesToText(bytestring):
+    """Unpack byte object to text string, assuming big-endian
+    byte order
+    """
 
     # Set encoding and error mode
     enc = "utf-8"
@@ -141,13 +154,13 @@ def bytesToText(bytes):
 
     try:
         # Decode to utf-8
-        string = bytes.decode(encoding=enc, errors=errorMode)
+        string = bytestring.decode(encoding=enc, errors=errorMode)
 
         # Remove control characters
         result = removeControlCharacters(string)
 
     except:
-        # Return empty string if bytes cannot be decoded
+        # Return empty string if bytestring cannot be decoded
         result = ""
 
-    return(result)
+    return result
