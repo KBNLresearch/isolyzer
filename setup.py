01065f28b4d87ee29641292f3e2d136a5b804dc0
bitsgalore <johan.vanderknijff@kb.nl>
2017-09-04 13:53:56 +0200

    added python_requires to setup.py

--- setup.py
+++ setup.py
@@ -5,27 +5,32 @@ import os
 import re
 from setuptools import setup, find_packages
 
+
 def read(*parts):
     """Read file and return contents"""
     path = os.path.join(os.path.dirname(__file__), *parts)
     with codecs.open(path, encoding='utf-8') as fobj:
         return fobj.read()
 
+
 def find_version(*file_paths):
     """Find and return version number"""
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
 INSTALL_REQUIRES = ['setuptools', 'six']
+PYTHON_REQUIRES = '>=2.7, !=3.0.*, !=3.1.*, <4'
 
 setup(name='isolyzer',
       packages=find_packages(),
       version=find_version('isolyzer', 'isolyzer.py'),
       license='Apache License 2.0',
       install_requires=INSTALL_REQUIRES,
+      python_requires=PYTHON_REQUIRES,
       platforms=['POSIX', 'Windows'],
       description='Verify size of ISO image',
       long_description='Verify size of ISO image for consistency with file system \
@@ -35,8 +40,8 @@ setup(name='isolyzer',
       maintainer='Johan van der Knijff',
       maintainer_email='johan.vanderknijff@kb.nl',
       url='https://github.com/KBNLresearch/isolyzer',
-      download_url='https://github.com/KBNLresearch/isolyzer/archive/' \
-        + find_version('isolyzer', 'isolyzer.py') + '.tar.gz',
+      download_url=('https://github.com/KBNLresearch/isolyzer/archive/' +
+                    find_version('isolyzer', 'isolyzer.py') + '.tar.gz'),
       package_data={'isolyzer': ['*.*']},
       entry_points={'console_scripts': [
           'isolyzer = isolyzer.isolyzer:main',
@@ -45,6 +50,5 @@ setup(name='isolyzer',
           'Environment :: Console',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-      ]
+          'Programming Language :: Python :: 3',]
      )
