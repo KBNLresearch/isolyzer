12f31932647c11aa6ed981590aeb7ca834d528a2
bitsgalore <johan.vanderknijff@kb.nl>
2022-04-07 17:19:09 +0200

    updated date

--- isolyzer/isolyzer.py
+++ isolyzer/isolyzer.py
@@ -3,7 +3,7 @@
 its filesystem-level headers.
 """
 #
-# Copyright (C) 2015 -2017, Johan van der Knijff, Koninklijke Bibliotheek
+# Copyright (C) 2015 -2022, Johan van der Knijff, Koninklijke Bibliotheek
 #  National Library of the Netherlands
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
