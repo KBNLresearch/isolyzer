350d1597fc460ce5c6c12a86e75b6d750541cac4
bitsgalore <johan.vanderknijff@kb.nl>
2022-04-07 20:29:45 +0200

    adjusted descriptors

--- makeTestfiles.sh
+++ makeTestfiles.sh
@@ -11,19 +11,19 @@ dataDir="$instDir"/dataTestfiles
 outDir="$instDir"/testFiles
 
 echo "ISO 9660 only"
-mkisofs -V "ISO 9660 only demo" -J -r -R -o $outDir/iso9660.iso $dataDir/
+mkisofs -V "ISO9660 only" -J -r -R -o $outDir/iso9660.iso $dataDir/
 
 echo "Hybrid ISO 9660/HFS file system"
-mkisofs -V "ISO 9660 + HFS hybrid demo" -J -r -R -hfs -o $outDir/iso9660_hfs.iso $dataDir/
+mkisofs -V "ISO9660/HFS" -J -r -R -hfs -o $outDir/iso9660_hfs.iso $dataDir/
 
 echo "Hybrid ISO 9660/HFS file system with partition table"
-mkisofs -V "ISO 9660 + HFS hybrid with partition table demo" -J -r -R -hfs -part -o $outDir/iso9660_hfs_part.iso $dataDir/
+mkisofs -V "ISO9660/HFS/part_table" -J -r -R -hfs -part -o $outDir/iso9660_hfs_part.iso $dataDir/
 
 echo "ISO 9660/HFS file system with Apple extensions"
-mkisofs -V "ISO 9660 + Apple Extensions demo" -J -r -R -apple -o $outDir/iso9660_apple.iso $dataDir/
+mkisofs -V "ISO9660/Apple_Extensions" -J -r -R -apple -o $outDir/iso9660_apple.iso $dataDir/
 
 echo "UDF Bridge (ISO 9660 / UDF hybrid)"
-mkisofs -V "UDF Bridge demo" -J -r -R -udf -o $outDir/iso9660_udf.iso $dataDir/
+mkisofs -V "UDF Bridge" -J -r -R -udf -o $outDir/iso9660_udf.iso $dataDir/
 
 echo "UDF (empty fs)"
 rm $outDir/udf.iso
@@ -33,12 +33,12 @@ mkudffs --media-type=dvd $outDir/udf.iso
 echo "HFS (empty fs)"
 rm $outDir/hfs.iso
 truncate -s 600K $outDir/hfs.iso
-mkfs.hfsplus -h -b 2048 -v "HFS demo" $outDir/hfs.iso
+mkfs.hfsplus -h -b 2048 -v "HFS" $outDir/hfs.iso
 
 echo "HFS Plus (empty fs)"
 rm $outDir/hfsplus.iso
 truncate -s 600K $outDir/hfsplus.iso
-mkfs.hfsplus -b 2048 -v "HFS Plus demo" $outDir/hfsplus.iso
+mkfs.hfsplus -b 2048 -v "HFS_Plus" $outDir/hfsplus.iso
 
 echo "Truncated ISO 9660 file"
 cp $outDir/iso9660.iso $outDir/iso9660_trunc.iso
