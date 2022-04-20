287e0e650f0a60dcaf40a3b70d1262db2ececea4
bitsgalore <johan.vanderknijff@kb.nl>
2022-04-20 13:30:17 +0200

    bumped version number to release candidate

--- isolyzer/isolyzer.py
+++ isolyzer/isolyzer.py
@@ -44,7 +44,7 @@ scriptPath, scriptName = os.path.split(sys.argv[0])
 if len(scriptName) == 0:
     scriptName = 'isolyzer'
 
-__version__ = '1.4.0a3'
+__version__ = '1.4.0rc1'
 
 
 # Name space and XSD schema strings 
